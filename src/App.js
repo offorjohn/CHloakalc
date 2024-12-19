fb79f1ec668830746791c7cb5916eaaf260b7412
offor <offorchukwuebuka3@gmail.com>
2024-12-19 13:56:34 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Home from './hompage';
 import About from './components/about';
-import Private from './components/private';
+import Private from './components/checkout';
 import Terms from './components/terms';
 const App = () => (
   <Routes>
 
    <Route path="/" element={<Home />} />
     <Route path="/about" element={<About />} />
-    <Route path="/private" element={<Private/>} />
+    <Route path="/checkout" element={<Private/>} />
     <Route path="/terms" element={<Terms/>} />
   </Routes> 
 );
